16b2e2344e43bc055d4717a893bb89f3a864e547
Wagner Scholl Lemos <josephblt@gmail.com>
2026-07-28 06:21:00 -0300

    Split authored cards into numeric batches

--- src/scripts/models/cards/feature_deck_builder.gd
+++ src/scripts/models/cards/feature_deck_builder.gd
@@ -2,7 +2,7 @@ class_name FeatureDeckBuilder
 extends RefCounted
 
 
-const DEFAULT_CARD_DATA_PATH: String = "res://data/cards/updates.json"
+const DEFAULT_CARD_DATA_PATH: String = "res://data/cards"
 
 
 static func build(

e8a7ea7283cb9aa6f755202e8de307c651982bc0
Wagner Scholl Lemos <josephblt@gmail.com>
2026-07-28 08:59:45 -0300

    Define Release model
    Closes #13

---
+++ src/scripts/models/lifecycle/release.gd
@@ -0,0 +1,80 @@
+class_name Release
+extends RefCounted
+
+
+const SPRINT_COUNT: int = 4
+
+var _number: int
+var _sprints: Array[Sprint]
+var _shipped: bool
+
+
+func _init(release_number: int) -> void:
+	_number = release_number
+	_sprints = []
+	_shipped = false
+
+
+func get_number() -> int:
+	return _number
+
+
+func get_sprints() -> Array[Sprint]:
+	return _sprints.duplicate()
+
+
+func get_sprint_count() -> int:
+	return _sprints.size()
+
+
+func is_full() -> bool:
+	return get_sprint_count() == SPRINT_COUNT
+
+
+func is_ready_to_ship() -> bool:
+	if not is_full():
+		return false
+
+	for sprint: Sprint in _sprints:
+		if not sprint.is_submitted():
+			return false
+
+	return true
+
+
+func is_shipped() -> bool:
+	return _shipped
+
+
+func add_sprint(sprint: Sprint) -> bool:
+	if _shipped or is_full() or sprint == null:
+		return false
+
+	_sprints.append(sprint)
+	return true
+
+
+func ship() -> bool:
+	if _shipped or not is_ready_to_ship():
+		return false
+
+	_shipped = true
+	return true
+
+
+func get_shipped_cards() -> Array[FeatureCard]:
+	var shipped_cards: Array[FeatureCard] = []
+
+	for sprint: Sprint in _sprints:
+		shipped_cards.append_array(sprint.get_selected_cards())
+
+	return shipped_cards
+
+
+func get_discarded_cards() -> Array[FeatureCard]:
+	var discarded_cards: Array[FeatureCard] = []
+
+	for sprint: Sprint in _sprints:
+		discarded_cards.append_array(sprint.get_discarded_cards())
+
+	return discarded_cards

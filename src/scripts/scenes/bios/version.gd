40b0042b2563d457cb77db9ce172fb3e01afb6aa
Wagner Scholl Lemos <josephblt@gmail.com>
2026-08-11 17:33:13 -0300

    Refactor version display nodes for clarity and consistency

--- src/scripts/scenes/bios/version.gd
+++ src/scripts/scenes/bios/version.gd
@@ -2,12 +2,12 @@ class_name BiosVersion
 extends BiosItem
 
 
-@onready var _version_label: Label = $VersionLeftVBoxContainer/VersionLine3
-@onready var _built_at_label: Label = $VersionLeftVBoxContainer/VersionLine4
-@onready var _build_type_label: Label = $VersionLeftVBoxContainer/VersionLine5
-@onready var _git_ref_link: LinkButton = $VersionLeftVBoxContainer/VersionLine6
-@onready var _commit_link: LinkButton = $VersionLeftVBoxContainer/VersionLine7
-@onready var _run_link: LinkButton = $VersionLeftVBoxContainer/VersionLine8
+@onready var _version_label: Label = %VersionLine3
+@onready var _built_at_label: Label = %BuiltAtLine4
+@onready var _build_type_label: Label = %BuildTypeLine5
+@onready var _git_ref_link: LinkButton = %GitRefLine6
+@onready var _commit_link: LinkButton = %CommitLine7
+@onready var _run_link: LinkButton = %RunLine8
 
 
 func _ready() -> void:
@@ -16,9 +16,9 @@ func _ready() -> void:
 	_version_label.text = "    Version: %s" % metadata.get_version_display()
 	_built_at_label.text = "    Built At: %s" % metadata.built_at
 	_build_type_label.text = "    Build Type: %s" % metadata.build_type
-	_set_metadata_link(_git_ref_link, "    Git Ref: %s" % metadata.git_ref, metadata.get_git_ref_url())
-	_set_metadata_link(_commit_link, "    Commit: %s" % metadata.commit_sha, metadata.get_commit_url())
-	_set_metadata_link(_run_link, "    Run: %s" % metadata.get_run_display(), metadata.get_run_url())
+	_set_metadata_link(_git_ref_link, "%s" % metadata.git_ref, metadata.get_git_ref_url())
+	_set_metadata_link(_commit_link, "%s" % metadata.commit_sha, metadata.get_commit_url())
+	_set_metadata_link(_run_link, "%s" % metadata.get_run_display(), metadata.get_run_url())
 
 
 func _set_metadata_link(link_button: LinkButton, link_text: String, link_uri: String) -> void:
